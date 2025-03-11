d76dee7e53b8e6fdfdddc7f9de2021d75df68933
amdymstfa <amdymoustapha011@gmail.com>
2025-03-11 11:46:09 +0000

    done: migrate rentals, payments,cars tables

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function showProfile()
+    {
+        return view('user.userProfil'); 
+    }
+
+    public function showDashboard()
+    {
+        return view('admin.dashboard'); 
+    }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashbord Admin</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            background-color: #DFDBE5;
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+<body>
+   
+</body>
+</html>
