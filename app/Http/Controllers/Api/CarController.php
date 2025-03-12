46167f6f9289a7bb38c1c0054d8f86eaeae2fc2b
amdymstfa <amdymoustapha011@gmail.com>
2025-03-12 12:37:48 +0000

    done: Add car creation API endpoint

---
+++ app/Http/Controllers/Api/CarController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Car;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'model' => ['required', 'string', 'min:3', 'max:255'],
+            'image_url' => ['nullable', 'string'], 
+            'is_available' => ['boolean'],
+            'daily_rate' => ['required', 'numeric', 'min:1']
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'All fields are required',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $cars = Car::create($request->all());
+        return response()->json([
+            'status' => true, 
+            'message' => 'Car created successfuly',
+            'data' => $cars
+        ], 201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/Car.php
+++ app/Models/Car.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'model' ,
+        'image_url', 
+        'is_available',
+        'daily_rate'
+    ];
 }
